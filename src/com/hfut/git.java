400f2094a149cdb443991e21e705953ccae8776e
chenzh <chenzhih@mail.hfut.edu.cn>
2019-10-30 19:25:39 +0800

    把本地工程提交到git

---
+++ src/com/hfut/git.java
@@ -0,0 +1,15 @@
+package com.hfut;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+    }
+}
